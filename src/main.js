27d5d57868ba2536f66273fffca01acb61ad3641
Uki7991 <tilek.kubanov@gmail.com>
2020-05-31 22:26:42 +0600

    routing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: Routes
+})
 
 //custom directives
 Vue.directive('theme', {
@@ -30,4 +37,5 @@ export const bus = new Vue();
 
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
